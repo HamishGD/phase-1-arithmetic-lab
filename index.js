52852f2af77fa699bd016a087b6006bb8f112266
hamish <hamishgeorgedavie@gmail.com>
2023-03-16 11:20:04 +1000

    Arthmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+const multiply = (num1 * num2);
+
+const random = Math.random() * 0 + 1;
+
+
+let num3 = 22;
+let num4 = 18;
+const mod = (num3 % num4) * num3 - num4;
+//=> 4
+
+const max = Math.ceil(10) * 2;
+
+
+
+
+
+
+
+
+
+
